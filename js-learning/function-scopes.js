f6e44346e8750a1c096cbef13f38a89ee30d3000
yilihamu-gh <yilihamu.y24@gmail.com>
2025-05-23 19:12:19 -0400

    for rebase

--- js-learning/function-scopes.js
+++ js-learning/function-scopes.js
@@ -1,7 +1,23 @@
-function myFunction() {
-    var localVar = "I'm local";
-    console.log(localVar); // Works fine
-}
+// function myFunction() {
+//     var localVar = "I'm local";
+//     console.log(localVar); // Works fine
+// }
 
-myFunction();
-console.log(localVar); // Error: localVar is not defined
+// myFunction();
+// console.log(localVar); // Error: localVar is not defined
+
+const myPromise = new Promise((correct, incorrect) => {
+    const dowload = false
+    if (dowload) {
+        correct({filename: "file.txt", size: "2MB"});
+    } else {
+        incorrect({error: "File not found"});
+    }
+});
+ 
+myPromise.then((result) => {
+    console.log("Promise resolved:", result);
+})
+.catch((error) => {
+    console.error("Promise rejected:", error);
+});
