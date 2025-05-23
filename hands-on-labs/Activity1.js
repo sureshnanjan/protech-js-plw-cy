d7b5011985fa5a112e5f7ee4fecf07c976c611b4
sureshnanjan <sureshnanjan@outlook.com>
2025-05-24 04:19:02 +0530

    Handson Lab Activity

---
+++ hands-on-labs/Activity1.js
@@ -0,0 +1,22 @@
+// TODO: Declare variables for the following test data:
+// - Test name: "Login Functionality Test"
+// - Number of test cases: 5
+// - Test is automated: true
+
+// TODO: Create a testConfig object with the following properties:
+// - browser: "Chrome"
+// - viewport: { width: 1280, height: 720 }
+// - timeout: 30000 (30 seconds)
+
+// TODO: Access and log the browser property from the testConfig
+
+// TODO: Update the timeout to 60000 (60 seconds)
+
+// TODO: Create a test summary string using template literals that includes:
+// - Test name
+// - Number of test cases
+// - Whether the test is automated
+// - Browser and viewport size
+// Expected output format: "Running [testName] with [numTestCases] test cases. Automated: [isAutomated]. Config: [browser] at [viewport.width]x[viewport.height]"
+
+console.log("Test summary will be logged here");
